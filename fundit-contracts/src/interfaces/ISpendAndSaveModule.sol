eef413d8a66dcf17cbbee0e4a26ab472d3e861df
Sendi John <johnsendi727@gmail.com>
2026-01-09 19:20:11 +0100

    feat: create interface for spend and save contract

---
+++ fundit-contracts/src/interfaces/ISpendAndSaveModule.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @title ISpendAndSaveModule
+ * @notice Interface for SpendAndSaveModule
+ */
+interface ISpendAndSaveModule {
+    struct SpendAndSaveConfig {
+        bool enabled;
+        bool isPercentage;
+        uint256 value;
+        uint256 minSpendThreshold;
+        uint256 dailyCap;
+        uint256 monthlyCap;
+        uint256 dailySaved;
+        uint256 monthlySaved;
+        uint256 lastResetDay;
+        uint256 lastResetMonth;
+        uint256 destinationId;
+        uint256 totalAutoSaved;
+        uint256 transactionCount;
+    }
+
+    function autoDepositSpendAndSave(address user, uint256 originalSpendAmount) external;
+    function getUserConfig(address user) external view returns (SpendAndSaveConfig memory);
+    function isSpendAndSaveEnabled(address user) external view returns (bool);
+}
